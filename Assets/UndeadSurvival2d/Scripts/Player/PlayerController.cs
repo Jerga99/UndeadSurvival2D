c3f57ddb9915ba758a465c772e17515e064f9ba6
Filip Jerga <jerga99@gmail.com>
2022-08-29 14:29:33 +0200

    Player Controller

---
+++ Assets/UndeadSurvival2d/Scripts/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Eincode.UndeadSurvival2d.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            Debug.Log("Init Player Controller");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y - Time.deltaTime * 2,
+                transform.position.z
+            );
+        }
+    }
+}
+
+
