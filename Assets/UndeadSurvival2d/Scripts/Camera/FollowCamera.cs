da24cdfec3ccfa668bfd075a5a18fbc6304e378b
Filip Jerga <jerga99@gmail.com>
2022-08-29 14:51:22 +0200

    Follow Camera

---
+++ Assets/UndeadSurvival2d/Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Eincode.UndeadSurvival2d.Camera
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        public Transform FollowTo;
+        public Vector3 Offset;
+        public float CameraDistance;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void LateUpdate()
+        {
+            transform.position = new Vector3(
+                FollowTo.position.x + Offset.x,
+                FollowTo.position.y + Offset.y,
+                CameraDistance
+            );
+        }
+    }
+}
+
