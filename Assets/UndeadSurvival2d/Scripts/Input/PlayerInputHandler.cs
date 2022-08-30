9f91f8b2b46cda922436463e0b5a40fa35afd2ab
Filip Jerga <jerga99@gmail.com>
2022-08-30 12:15:33 +0200

    Player Input Handler

---
+++ Assets/UndeadSurvival2d/Scripts/Input/PlayerInputHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+namespace Eincode.UndeadSurvival2d.Input
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        public void OnMove(InputValue value)
+        {
+            Debug.Log(value.Get<Vector2>());
+        }
+    }
+}
+
