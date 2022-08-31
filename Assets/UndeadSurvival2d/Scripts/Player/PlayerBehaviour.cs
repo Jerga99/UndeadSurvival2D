bea6ab1d3eaf14fb47c6aadc414bca9e9c518d84
Filip Jerga <jerga99@gmail.com>
2022-08-31 09:17:42 +0200

    Set Animator Speed

---
+++ Assets/UndeadSurvival2d/Scripts/Player/PlayerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Eincode.UndeadSurvival2d.Player
+{
+    public class PlayerBehaviour : MonoBehaviour
+    {
+        private PlayerController _playerController;
+        private Animator _animator;
+
+        // Use this for initialization
+        void Start()
+        {
+            _playerController = GetComponent<PlayerController>();
+            _animator = GetComponentInChildren<Animator>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var speed = Mathf.Round(_playerController.movementBlend * 100f) / 100f;
+            _animator.SetFloat("Speed", speed);
+        }
+    }
+}
+
