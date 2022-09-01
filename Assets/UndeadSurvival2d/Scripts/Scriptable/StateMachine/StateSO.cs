f05272ceda6f6860e2b185b3f76a8d0f745a1f07
Filip Jerga <jerga99@gmail.com>
2022-09-01 10:08:29 +0200

    StateSO init

---
+++ Assets/UndeadSurvival2d/Scripts/Scriptable/StateMachine/StateSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+
+namespace Eincode.UndeadSurvival2d.Scriptable
+{
+    [CreateAssetMenu(fileName = "StateSO", menuName = "StateMachine/New State")]
+    public class StateSO : ScriptableObject
+    {
+        public string Name;
+    }
+
+
+    public class State
+    {
+
+    }
+
+
+}
--- Assets/UndeadSurvival2d/Scripts/StateMachine/StateMachine.cs
+++ Assets/UndeadSurvival2d/Scripts/StateMachine/StateMachine.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Eincode.UndeadSurvival2d.Scriptable;
 
 namespace Eincode.UndeadSurvival2d.StateMachine
 {
     public class StateMachine : MonoBehaviour
     {
+        [SerializeField]
+        private StateSO[] _statesSO;
+
         // Use this for initialization
         void Start()
         {
             Debug.Log("Init StateMachine");
+            Debug.Log(_statesSO[0].Name);
         }
 
         // Update is called once per frame
