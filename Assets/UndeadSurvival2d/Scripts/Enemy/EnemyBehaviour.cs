b74ae2219d15a162945941a85b604400f703266d
Filip Jerga <jerga99@gmail.com>
2022-09-01 09:44:26 +0200

    State Machine Start

---
+++ Assets/UndeadSurvival2d/Scripts/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Eincode.UndeadSurvival2d.Character;
+
+namespace Eincode.UndeadSurvival2d.Enemy
+{
+    public class EnemyBehaviour : CharacterBehaviour
+    {
+        new void Start()
+        {
+            base.Start();
+            Debug.Log("Init EnemyBehaviour");
+        }
+
+        void Update()
+        {
+
+        }
+    }
+
+
+}
---
+++ Assets/UndeadSurvival2d/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Eincode.UndeadSurvival2d.StateMachine
+{
+    public class StateMachine : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+            Debug.Log("Init StateMachine");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+
+}
