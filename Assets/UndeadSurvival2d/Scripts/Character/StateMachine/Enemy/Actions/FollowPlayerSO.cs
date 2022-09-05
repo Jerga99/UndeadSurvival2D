17dadca41810bb4ab1fc9f06f68871795244bfda
Filip Jerga <jerga99@gmail.com>
2022-09-05 10:17:55 +0200

    Follow Player Action

---
+++ Assets/UndeadSurvival2d/Scripts/Character/StateMachine/Enemy/Actions/FollowPlayerSO.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+using UnityEngine;
+using Eincode.UndeadSurvival2d.StateMachine;
+using Eincode.UndeadSurvival2d.StateMachine.Scriptable;
+
+[CreateAssetMenu(
+    fileName = "FollowPlayerSO",
+    menuName = "StateMachine/Enemy/Actions/Follow Player")]
+public class FollowPlayerSO : StateActionSO
+{
+    public override StateAction CreateAction()
+    {
+        return new FollowPlayer();
+    }
+}
+
+public class FollowPlayer : StateAction
+{
+}
+
--- Assets/UndeadSurvival2d/Scripts/StateMachine/Scriptable/StateActionSO.cs
+++ Assets/UndeadSurvival2d/Scripts/StateMachine/Scriptable/StateActionSO.cs
@@ -1,11 +1,17 @@
 ﻿
-
-
+using UnityEngine;
 
 namespace Eincode.UndeadSurvival2d.StateMachine.Scriptable
 {
-    abstract public class StateActionSO
+    abstract public class StateActionSO : ScriptableObject
     {
+        public StateAction GetAction()
+        {
+            var action = CreateAction();
+            action.originSO = this;
+            return action;
+        }
 
+        public abstract StateAction CreateAction();
     }
 }
