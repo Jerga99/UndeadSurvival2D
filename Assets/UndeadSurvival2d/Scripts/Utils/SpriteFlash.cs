cc82d10ea3da026786c44047725dd9992fe87e42
Filip Jerga <jerga99@gmail.com>
2022-09-07 09:29:41 +0200

    Sprite Flash Start

---
+++ Assets/UndeadSurvival2d/Scripts/Utils/SpriteFlash.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace Eincode.UndeadSurvival2d.Utils
+{
+    public class SpriteFlash : MonoBehaviour
+    {
+        public Material flashMaterial;
+
+        private SpriteRenderer _spriteRenderer;
+        private Material _originalMaterial;
+
+        void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _originalMaterial = _spriteRenderer.material;
+            flashMaterial = new Material(flashMaterial);
+        }
+    }
+}
