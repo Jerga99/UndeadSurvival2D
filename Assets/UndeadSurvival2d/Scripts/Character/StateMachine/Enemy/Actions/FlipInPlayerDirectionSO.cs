b198693ad0edc1ceb1d9690187a66dedfe132c43
Filip Jerga <jerga99@gmail.com>
2022-09-08 08:31:24 +0200

    Flip in player direction action

---
+++ Assets/UndeadSurvival2d/Scripts/Character/StateMachine/Enemy/Actions/FlipInPlayerDirectionSO.cs
@@ -0,0 +1,33 @@
+﻿using Eincode.UndeadSurvival2d.StateMachine;
+using Eincode.UndeadSurvival2d.StateMachine.Scriptable;
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "FlipInPlayerDirectionSO",
+    menuName = "StateMachine/Enemy/Actions/Flip in player direction"
+)]
+public class FlipInPlayerDirectionSO : StateActionSO
+{
+    public override StateAction CreateAction()
+    {
+        return new FlipInPlayerDirection();
+    }
+}
+
+public class FlipInPlayerDirection : StateAction
+{
+
+    public override void Awake(StateMachineCore stateMachine)
+    {
+    }
+
+    public override void OnEnter()
+    {
+    }
+
+    public override void OnUpdate()
+    {
+        Debug.Log("Updating Fliping!");
+    }
+}
+
