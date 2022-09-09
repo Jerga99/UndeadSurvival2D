40b386205727e22cf6f99997ad7da95ed2e8c075
Filip Jerga <jerga99@gmail.com>
2022-09-09 10:41:14 +0200

    Add Health to Damageable

--- Assets/UndeadSurvival2d/Scripts/Character/Damageable.cs
+++ Assets/UndeadSurvival2d/Scripts/Character/Damageable.cs
@@ -9,18 +9,31 @@ namespace Eincode.UndeadSurvival2d.Character
         public Color FlashDamageColor;
         public ParticleSystem ParticleHitEffect;
 
+        [SerializeField]
+        private IntValueSO _initialHealthSO;
+
+        [SerializeField]
+        [Header("Set only for Player")]
+        private HealthSO _healthSO;
+
         private SpriteFlash _flashDamageEffect;
+
+        private void Awake()
+        {
+            // Applies only for enemies
+            if (_healthSO == null)
+            {
+                _healthSO = ScriptableObject.CreateInstance<HealthSO>();
+            }
+
+            _healthSO.CurrentHealth = _healthSO.MaxHealth = _initialHealthSO.InitialValue;
+
+        }
 
         // Use this for initialization
         void Start()
         {
             _flashDamageEffect = GetComponentInChildren<SpriteFlash>();
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
         }
 
         public void TakeDamage(int damage)
