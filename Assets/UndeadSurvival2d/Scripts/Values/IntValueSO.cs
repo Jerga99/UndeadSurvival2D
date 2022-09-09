206d93bf231cf81bae148437125a89b85c695c6e
Filip Jerga <jerga99@gmail.com>
2022-09-09 09:41:11 +0200

    Int Value SO

---
+++ Assets/UndeadSurvival2d/Scripts/Values/IntValueSO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IntValue", menuName = "Values/Int Value")]
+public class IntValueSO : ScriptableObject, ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private int _initialValue;
+
+    [NonSerialized]
+    public int RuntimeValue;
+
+    public int InitialValue => _initialValue;
+
+    public void ResetValue()
+    {
+        RuntimeValue = InitialValue;
+    }
+
+    public void OnBeforeSerialize() { }
+
+    public void OnAfterDeserialize()
+    {
+        ResetValue();
+    }
+}
+
