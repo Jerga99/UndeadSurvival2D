f4f51e432cf3b18e5b04e92bbb7efafa282ea347
Filip Jerga <jerga99@gmail.com>
2022-09-09 10:23:53 +0200

    Player Health

---
+++ Assets/UndeadSurvival2d/Scripts/Values/Health/HealthSO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "CharacterHealth",
+    menuName = "Values/Character's Health"
+)]
+public class HealthSO : ScriptableObject
+{
+    [SerializeField]
+    private int _maxHealth;
+
+    [SerializeField]
+    private int _currentHealth;
+
+    public int MaxHealth { get => _maxHealth; set { _maxHealth = value; } }
+    public int CurrentHealth { get => _currentHealth; set { _currentHealth = value; } }
+
+    public void InflictDamage(int damage)
+    {
+        if (_currentHealth > 0)
+        {
+            _currentHealth -= damage;
+        }
+        else
+        {
+            _currentHealth = 0;
+        }
+    }
+}
+
