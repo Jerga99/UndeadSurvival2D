1fb87587987f179e3fbb1c78802914e05709a55b
Filip Jerga <jerga99@gmail.com>
2022-09-09 10:56:08 +0200

    Slider Bar

---
+++ Assets/UndeadSurvival2d/Scripts/UI/SliderBar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Eincode.UndeadSurvival2d.UI
+{
+    public class SliderBar : MonoBehaviour
+    {
+        public Slider Slider;
+
+        void Start()
+        {
+            SetValue(100, 100);
+        }
+
+        public void SetValue(float value, float maxValue)
+        {
+            Slider.value = value;
+            Slider.maxValue = maxValue;
+        }
+    }
+}
