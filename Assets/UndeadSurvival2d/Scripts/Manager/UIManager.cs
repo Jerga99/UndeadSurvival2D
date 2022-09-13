960884558a8e49d4c5f4662c42099d6a6ddb3f52
Filip Jerga <jerga99@gmail.com>
2022-09-13 09:33:14 +0200

    UI Manager

---
+++ Assets/UndeadSurvival2d/Scripts/Manager/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Eincode.UndeadSurvival2d.Manager
+{
+    public class UIManager : MonoBehaviour
+    {
+        public static UIManager Instance { get; private set; }
+
+        public RectTransform DamageCanvas;
+        public GameObject DamageTextPrefab;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            Instantiate(DamageTextPrefab, DamageCanvas.transform);
+        }
+    }
+}
