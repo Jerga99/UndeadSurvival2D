e39c8838448c7180755c43858c72a440049fa583
Filip Jerga <jerga99@gmail.com>
2022-09-13 10:09:20 +0200

    Provide correct position for damage

--- Assets/UndeadSurvival2d/Scripts/UI/DamageDisplay.cs
+++ Assets/UndeadSurvival2d/Scripts/UI/DamageDisplay.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using TMPro;
 
+using UnityCamera = UnityEngine.Camera;
+
 namespace Eincode.UndeadSurvival2d.UI
 {
     public class DamageDisplay : MonoBehaviour
@@ -10,6 +12,26 @@ namespace Eincode.UndeadSurvival2d.UI
         private Transform _target;
         private RectTransform _canvas;
 
+        private void Update()
+        {
+            if (_target == null) { return; }
+
+            float offsetPosY = _target.transform.position.y + 0.1f;
+            var offsetPosition = new Vector3(
+                _target.transform.position.x,
+                offsetPosY,
+                _target.transform.position.z
+            );
+
+            var screenPoint = UnityCamera.main.WorldToScreenPoint(offsetPosition);
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                _canvas,
+                screenPoint,
+                null,
+                out Vector2 localPosition
+            );
+            transform.localPosition = localPosition;
+        }
 
         public void ShowDamage(
             int damage,
