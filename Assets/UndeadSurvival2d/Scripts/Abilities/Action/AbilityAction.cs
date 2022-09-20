50858bac69782f92d1a9b494e94a00f33f51a3bf
Filip Jerga <jerga99@gmail.com>
2022-09-20 10:38:19 +0700

    Collistion Layer

--- Assets/UndeadSurvival2d/Scripts/Abilities/Action/AbilityAction.cs
+++ Assets/UndeadSurvival2d/Scripts/Abilities/Action/AbilityAction.cs
@@ -7,9 +7,14 @@ namespace Eincode.UndeadSurvival2d.Abilities.Action
 {
     public class AbilityAction : MonoBehaviour
     {
+        public LayerMask CollideWith;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            Debug.Log("Attack has landed!");
+            if ((CollideWith.value & 1 << collision.transform.gameObject.layer) > 0)
+            {
+                Debug.Log("Attack has landed!");
+            }
         }
     }
 }
