9b9bebbacbd296c011243452d55e7443c8170738
Filip Jerga <jerga99@gmail.com>
2022-09-20 10:05:24 +0700

    Ability Action

---
+++ Assets/UndeadSurvival2d/Scripts/Abilities/Action/AbilityAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+
+
+namespace Eincode.UndeadSurvival2d.Abilities.Action
+{
+    public class AbilityAction : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log("Attack has landed!");
+        }
+    }
+}
+
+
