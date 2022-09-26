5fff0277ba508de23a084afda0ffc8de9a78a7cf
Filip Jerga <jerga99@gmail.com>
2022-09-27 07:23:40 +0800

    Pick the coin

---
+++ Assets/UndeadSurvival2d/Scripts/Reward/RewardBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Eincode.UndeadSurvival2d.Reward
+{
+    public class RewardBehaviour : MonoBehaviour
+    {
+        public LayerMask InteractWith;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if ((InteractWith.value & 1 << collision.gameObject.layer) > 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
