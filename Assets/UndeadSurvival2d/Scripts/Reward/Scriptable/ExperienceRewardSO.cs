aa0d44db65a55f326ac8cb4edd828d971146f6ff
Filip Jerga <jerga99@gmail.com>
2022-09-27 08:01:08 +0800

    Experience SO

---
+++ Assets/UndeadSurvival2d/Scripts/Reward/Scriptable/ExperienceRewardSO.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+using UnityEngine;
+
+namespace Eincode.UndeadSurvival2d.Reward.Scriptable
+{
+    [CreateAssetMenu(
+        fileName = "ExperienceSO",
+        menuName = "Rewards/Experience Rewards"
+    )]
+    public class ExperienceRewardSO : RewardSO
+    {
+        public int experience;
+    }
+}
+
+
---
+++ Assets/UndeadSurvival2d/Scripts/Reward/Scriptable/RewardSO.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+namespace Eincode.UndeadSurvival2d.Reward.Scriptable
+{
+    public abstract class RewardSO : ScriptableObject
+    {
+        public GameObject RewardPrefab;
+
+        public virtual void Drop(MonoBehaviour source)
+        {
+            Instantiate(RewardPrefab, source.transform.position, Quaternion.identity);
+        }
+    }
+}
+
+
