5dc7e2ace35b2f3fa006a2439a3a00d425cd4e73
Filip Jerga <jerga99@gmail.com>
2022-10-03 07:33:55 +0800

    Projectile Ability

---
+++ Assets/UndeadSurvival2d/Scripts/Character/Abilities/ProjectileAbilitySO.cs
@@ -0,0 +1,38 @@
+﻿
+
+using UnityEngine;
+using Eincode.UndeadSurvival2d.Abilities;
+using Eincode.UndeadSurvival2d.Abilities.Scriptable;
+
+[CreateAssetMenu(
+    fileName = "ProjectileAbilitySO",
+    menuName = "Abilities/ProjectileAbility"
+)]
+public class ProjectileAbilitySO : AbilitySO
+{
+    protected override Ability CreateAbility()
+    {
+        return new ProjectileAbility();
+    }
+}
+
+public class ProjectileAbility : Ability
+{
+    public override void Awake(AbilityRunner runner)
+    {
+        base.Awake(runner);
+    }
+
+    public override void TriggerAbility(AbilityRunner runner)
+    {
+        var abilityGO = InstantiateAbility(runner);
+        abilityGO.transform.parent = runner.transform;
+    }
+
+    public override void Run()
+    {
+        base.Run();
+        Debug.Log("Casting Projectile!");
+    }
+}
+
