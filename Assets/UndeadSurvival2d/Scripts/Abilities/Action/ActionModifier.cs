da7863eaa921c32508d3199ecf02c4671b4257df
Filip Jerga <jerga99@gmail.com>
2022-10-03 08:46:42 +0800

    Action Modifier

---
+++ Assets/UndeadSurvival2d/Scripts/Abilities/Action/ActionModifier.cs
@@ -0,0 +1,15 @@
+﻿
+using Eincode.UndeadSurvival2d.Abilities.Scriptable;
+
+namespace Eincode.UndeadSurvival2d.Abilities.Action
+{
+    public abstract class ActionModifier
+    {
+        public ActionModifierSO originSO;
+
+        public abstract void Awake(AbilityAction action);
+        public abstract void Update(AbilityAction action);
+    }
+}
+
+
---
+++ Assets/UndeadSurvival2d/Scripts/Abilities/Scriptable/ActionModifierSO.cs
@@ -0,0 +1,23 @@
+﻿
+using Eincode.UndeadSurvival2d.Abilities.Action;
+using UnityEngine;
+
+namespace Eincode.UndeadSurvival2d.Abilities.Scriptable
+{
+    public abstract class ActionModifierSO : ScriptableObject
+    {
+        public virtual ActionModifier GetActionModifier(AbilityAction action)
+        {
+            var modifier = CreateActionModifier();
+
+            modifier.Awake(action);
+            modifier.originSO = this;
+
+            return modifier;
+        }
+
+        public abstract ActionModifier CreateActionModifier();
+    }
+}
+
+
