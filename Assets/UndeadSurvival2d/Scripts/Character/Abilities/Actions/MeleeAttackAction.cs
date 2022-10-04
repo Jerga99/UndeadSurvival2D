3e9fc69439a3e4b7bafa105c443bf77d486c16ac
Filip Jerga <jerga99@gmail.com>
2022-10-04 12:43:17 +0800

    Flip Melee Attack

--- Assets/UndeadSurvival2d/Scripts/Character/Abilities/Actions/MeleeAttackAction.cs
+++ Assets/UndeadSurvival2d/Scripts/Character/Abilities/Actions/MeleeAttackAction.cs
@@ -1,9 +1,22 @@
 ﻿
 
-
+using UnityEngine;
+using Eincode.UndeadSurvival2d.Manager;
 
 public class MeleeAttackAction : CollisionAction
 {
+    private SpriteRenderer _sprite;
 
+    protected override void Start()
+    {
+        base.Start();
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        _sprite.flipX = GameManager.Instance.GetPlayer().GetFlipX();
+    }
 }
 
