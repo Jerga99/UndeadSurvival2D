314502c684cb2f7c7a9b9ec495a758c4029110d1
Filip Jerga <jerga99@gmail.com>
2022-10-05 08:30:06 +0800

    Go to next round

--- Assets/UndeadSurvival2d/Scripts/Spawning/EnemySpawner.cs
+++ Assets/UndeadSurvival2d/Scripts/Spawning/EnemySpawner.cs
@@ -11,21 +11,36 @@ namespace Eincode.UndeadSurvival2d.Spawning
         public SpawnConfigSO.SpawnConfig Wave => _spawnConfigSO.Waves[GameStage];
         public bool SpawnTick => _spawnIntervalDelta >= _spawnConfigSO.SpawnIntervalTime;
 
+        public bool IsRoundFinished => _roundTime >= Wave.RoundTime;
+        public bool IsThereNextRound => GameStage < _spawnConfigSO.Waves.Count - 1;
+
         [SerializeField]
         private SpawnConfigSO _spawnConfigSO;
 
         private float _spawnIntervalDelta;
-
+        private float _roundTime;
 
         private void Update()
         {
             _spawnIntervalDelta += Time.deltaTime;
+            _roundTime += Time.deltaTime;
 
             if (SpawnTick)
             {
                 Spawn();
                 _spawnIntervalDelta = 0;
             }
+            else if (IsThereNextRound && IsRoundFinished)
+            {
+                GoToNextRound();
+                Debug.Log("Going to next round!");
+            }
+        }
+
+        private void GoToNextRound()
+        {
+            GameStage += 1;
+            _spawnIntervalDelta = _roundTime = 0;
         }
 
         private void Spawn()
