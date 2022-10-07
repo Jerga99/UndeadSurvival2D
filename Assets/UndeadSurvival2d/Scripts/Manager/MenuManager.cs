54977dcf0596592e64633d3eb8c05437b1666e9b
Filip Jerga <jerga99@gmail.com>
2022-10-07 10:39:58 +0800

    Menu Manager

---
+++ Assets/UndeadSurvival2d/Scripts/Manager/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace Eincode.UndeadSurvival2d.Manager
+{
+    public class MenuManager : MonoBehaviour
+    {
+        public static MenuManager Instance { get; private set; }
+
+        public GameObject InitialOpen;
+
+        private GameObject _openMenu;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            OpenMenu(InitialOpen);
+        }
+
+        public void OpenMenu(GameObject menu)
+        {
+            if (_openMenu == menu)
+            {
+                return;
+            }
+
+            menu.SetActive(true);
+            _openMenu = menu;
+        }
+    }
+}
