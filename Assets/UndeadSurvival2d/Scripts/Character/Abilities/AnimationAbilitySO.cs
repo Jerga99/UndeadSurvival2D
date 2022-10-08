7914eb21945abdc8abc65173bbefcd06c10be2b2
Filip Jerga <jerga99@gmail.com>
2022-10-08 08:43:31 +0800

    Animation Ability

---
+++ Assets/UndeadSurvival2d/Scripts/Character/Abilities/AnimationAbilitySO.cs
@@ -0,0 +1,50 @@
+﻿
+
+using UnityEngine;
+using System;
+using Eincode.UndeadSurvival2d.Abilities;
+using Eincode.UndeadSurvival2d.Abilities.Scriptable;
+
+[CreateAssetMenu(
+    fileName = "AnimationAbilitySO",
+    menuName = "Abilities/Animation Ability"
+)]
+public class AnimationAbilitySO : AbilitySO
+{
+    public string AnimationParameter;
+
+    protected override Ability CreateAbility()
+    {
+        return new AnimationAbility(AnimationParameter);
+    }
+}
+
+public class AnimationAbility : Ability
+{
+    private Animator _animator;
+    private int _animId;
+
+    public AnimationAbility(string animationParameter)
+    {
+        _animId = Animator.StringToHash(animationParameter);
+    }
+
+    public override void Awake(AbilityRunner runner)
+    {
+        base.Awake(runner);
+        _animator = runner.GetComponentInChildren<Animator>();
+    }
+
+    public override void TriggerAbility()
+    {
+        _animator.SetTrigger(_animId);
+        Debug.Log("Triggering Animation of Evade");
+    }
+
+    public override void Run()
+    {
+        base.Run();
+        TriggerAbility();
+    }
+}
+
