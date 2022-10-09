cef269b11521578418f0421de41b1c6eb66a3028
Filip Jerga <jerga99@gmail.com>
2022-10-10 07:12:52 +0800

    Evade animation event

--- Assets/UndeadSurvival2d/Scripts/Character/Abilities/AnimationAbilitySO.cs
+++ Assets/UndeadSurvival2d/Scripts/Character/Abilities/AnimationAbilitySO.cs
@@ -13,9 +13,12 @@ public class AnimationAbilitySO : AbilitySO
 {
     public string AnimationParameter;
 
+    [SerializeField]
+    public VoidEventChannelSO _evadeAnimationEvent;
+
     protected override Ability CreateAbility()
     {
-        return new AnimationAbility(AnimationParameter);
+        return new AnimationAbility(AnimationParameter, _evadeAnimationEvent);
     }
 }
 
@@ -24,9 +27,10 @@ public class AnimationAbility : Ability
     private Animator _animator;
     private int _animId;
 
-    public AnimationAbility(string animationParameter)
+    public AnimationAbility(string animationParameter, VoidEventChannelSO evadeEvent)
     {
         _animId = Animator.StringToHash(animationParameter);
+        evadeEvent.OnEventRaised += OnEvadeEventFinished;
     }
 
     public override void Awake(AbilityRunner runner)
@@ -44,6 +48,11 @@ public class AnimationAbility : Ability
     {
         base.Run();
         TriggerAbility();
+    }
+
+    private void OnEvadeEventFinished()
+    {
+        Debug.Log("Evade Animation Finished!");
     }
 }
 
---
+++ Assets/UndeadSurvival2d/Scripts/SMB/EvadeAnimationOnExitSMB.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class EvadeAnimationOnExitSMB : StateMachineBehaviour
+{
+
+    [Header("Channeling")]
+    [SerializeField]
+    private VoidEventChannelSO _evadeAnimationEvent;
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (_evadeAnimationEvent != null)
+        {
+            Debug.Log("Evade Animation Event Raised!");
+            _evadeAnimationEvent.RaiseEvent();
+        }
+    }
+}
+
